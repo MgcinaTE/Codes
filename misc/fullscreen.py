50fcaf73d71868488f99783432a48e1b8bbdc9b0
MgcinaTE <te.mgcina@gmail.com>
2023-08-18 02:13:03 +0200

    Add fullscreen window and subprocess integration

--- misc/fullscreen.py
+++ misc/fullscreen.py
@@ -1,19 +1,16 @@
+#If you want to integrate this code with your previous code that uses the subprocess module, you can do so by combining them within a single script. Here's how you might integrate both code snippets:
 import sys
+import subprocess
 
-if (
-    sys.version_info[0] == 2
-):  # Just checking your Python version to import Tkinter properly.
+if sys.version_info[0] == 2:
     from Tkinter import *
 else:
     from tkinter import *
 
-
 class Fullscreen_Window:
     def __init__(self):
         self.tk = Tk()
-        self.tk.wm_state(
-            "zoomed"
-        )  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
+        self.tk.wm_state("zoomed")
         self.frame = Frame(self.tk)
         self.frame.pack()
         self.state = False
@@ -21,7 +18,7 @@ class Fullscreen_Window:
         self.tk.bind("<Escape>", self.end_fullscreen)
 
     def toggle_fullscreen(self, event=None):
-        self.state = not self.state  # Just toggling the boolean
+        self.state = not self.state
         self.tk.attributes("-fullscreen", self.state)
         return "break"
 
@@ -30,7 +27,32 @@ class Fullscreen_Window:
         self.tk.attributes("-fullscreen", False)
         return "break"
 
-
 if __name__ == "__main__":
     w = Fullscreen_Window()
     w.tk.mainloop()
+
+    proc = subprocess.Popen(
+        [
+            "C:/Users/aivar/Desktop/plink.exe",
+            "-ssh",
+            #"-t",
+            "-batch",
+            "-pw",
+            "pi-top",
+            "pi@pi-top",
+            "python3",
+            "/home/pi/Desktop/prob.py",
+        ],
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        universal_newlines=True,
+    )
+
+    while True:
+        c = proc.stdout.read(1)
+        print(c, end="")
+        if c == ":":
+            print("ennekirj")
+            proc.stdin.write("Aivar\n")
+            proc.stdin.flush()
+            print("pealekirj")
